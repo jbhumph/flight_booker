0547d7aa06dd9dddb8dea768ffd27512625502a4
John <jbhumph@gmail.com>
2025-01-31 09:31:30 -0800

    created basic logic for Plane and Seat objects.

--- plane/Plane.java
+++ plane/Plane.java
@@ -1,5 +1,31 @@
 package plane;
 
 public class Plane {
-    
+    public Plane() {
+        // plane has 20 rows and 4 seats per row
+        Seat[][] plane = new Seat[20][4];
+        // create seats for each row
+        boolean window = false;
+        String pClass = "";
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 4; j++) {
+                // create window seats
+                if (j == 0 || j == 3) {
+                    window = true;
+                } else {
+                    window = false;
+                }
+                // create class
+                if (i < 5) {
+                    pClass = "First";
+                } else if (i < 15) {
+                    pClass = "Business";
+                } else {
+                    pClass = "Economy";
+                }
+                plane[i][j] = new Seat((char) (i + 65), j + 1, window, pClass);
+            }
+        }
+
+    }
 }
--- plane/Seat.java
+++ plane/Seat.java
@@ -1,5 +1,23 @@
 package plane;
 
 public class Seat {
-    
+    private char row;
+    private int seat;
+    private boolean available;
+    private int passenger;
+    private boolean window;
+    private boolean priority;
+    private String pClass;
+
+    public Seat(char row, int seat, boolean window, String pClass) {
+        this.row = row;
+        this.seat = seat;
+        this.available = true;
+        this.passenger = 0;
+        this.window = window;
+        this.priority = false;
+        this.pClass = pClass;
+    }
+
+
 }
